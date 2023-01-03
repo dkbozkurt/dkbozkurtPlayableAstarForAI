8f6505883c6db6a2ca19ff82c37664ebbdeb0df8
Doğukan Bozkurt <dogukanbozkurt@panteon.games>
2023-01-03 10:35:25 +0300

    Inıtial fork with adapt to 2020.1.17 version

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+  private ZeroLayer zeroLayer;
+
+  void Start() {
+    zeroLayer = FindObjectOfType<ZeroLayer>();
+  }
+
+  void OnMouseOver() {
+    if (Input.GetMouseButton(0))
+      zeroLayer.otherCube = gameObject;
+  }
+}
---
+++ Assets/Scripts/ZeroLayer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handles Raycast-detection
+public class ZeroLayer : MonoBehaviour {
+  public GameObject otherCube;
+  private Grid grid;
+
+  void Start() {
+    grid = FindObjectOfType<Grid>();
+  }
+
+  void Update() {
+    if (!grid.isCalculating) {
+      if (Input.GetMouseButton(0)) {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100)) {
+          if (otherCube) {
+            otherCube.GetComponent<Collider>().enabled = false;
+            otherCube.transform.position = FindClosestCell(hit.point) + new Vector3(0, 1, 0);
+          }
+        }
+      } else if (Input.GetMouseButtonUp(0)) {
+        if (otherCube) {
+          otherCube.GetComponent<Collider>().enabled = true;
+          otherCube = null;
+
+          grid.CalculatePathExternal();
+        }
+      }
+    }
+  }
+
+  private Vector3 FindClosestCell(Vector3 startPosition) {
+    GameObject closest = null;
+    float distance = Mathf.Infinity;
+    Vector3 position = startPosition;
+    foreach (GameObject gameObject in grid.allCells) {
+      Vector3 diff = gameObject.transform.position - position;
+      float curDistance = diff.sqrMagnitude;
+      if (curDistance < distance) {
+        closest = gameObject;
+        distance = curDistance;
+      }
+    }
+    return closest.transform.position;
+  }
+}
